6a9bbf24101aa82da61ceeb7c8955ec0aa8265d9
greghorne <greghorne@hotmail.com>
2019-01-08 19:34:51 +0000

    files updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+// init app
 var express = require('express');
 var app     = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-// index page
+// use res.render to load up an ejs view file index page
 app.get('/', function(req, res) {
 	res.render('pages/index');
 });
@@ -28,5 +28,5 @@ app.get('/', function (req, res) {
 });
 
 var server = app.listen(process.env.APP_PORT, process.env.APP_HOST, function () {
-   console.log("Example app listening at http://%s:%s", server.address().address, server.address().port)
+   console.log("App listening at http://%s:%s", server.address().address, server.address().port)
 })
