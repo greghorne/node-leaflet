39a1659376717ec1368f25dbc9364d003cccf52b
greghorne <greghorne@hotmail.com>
2019-01-10 19:46:55 +0000

    added js file to hold constants

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+CONST_MAP_URL           = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+CONST_MAP_ATTRIBUTION   = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
+CONST_MAP_MAX_ZOOM      =  19,
+CONST_MAP_LATITUDEY     =  38.62693,
+CONST_MAP_LONGITUDEX    = -96.02274,
+CONST_MAP_DEFAULT_ZOOM  =   5
