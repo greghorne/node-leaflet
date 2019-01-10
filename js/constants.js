8dae92345d6991cf78af8f90f5b0a6751f9a6ea8
greghorne <greghorne@hotmail.com>
2019-01-10 20:44:57 +0000

    renamed constants

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,7 @@
-CONST_MAP_URL           = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-CONST_MAP_ATTRIBUTION   = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
-CONST_MAP_MAX_ZOOM      =  19,
-CONST_MAP_LATITUDEY     =  38.62693,
-CONST_MAP_LONGITUDEX    = -96.02274,
-CONST_MAP_DEFAULT_ZOOM  =   5
+// map settings to define the leaflet base map
+CONST_MAP_URL               = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+CONST_MAP_ATTRIBUTION       = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>',
+CONST_MAP_MAX_ZOOM          =  19,
+CONST_MAP_CENTER_LATITUDEY  =  38.62693,
+CONST_MAP_CENTER_LONGITUDEX = -96.02274,
+CONST_MAP_INITIAL_ZOOM      =   5
